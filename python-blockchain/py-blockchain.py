25f2eaa7da7181c549c606e7d80602b24dbc69b8
Amirmohammad Kazemi <amirmohammadkazemi2002@gmail.com>
2024-01-22 21:09:46 -0500

    new_trx method

--- python-blockchain/py-blockchain.py
+++ python-blockchain/py-blockchain.py
@@ -1,15 +1,18 @@
-class blockchain(object):
+class blockchain():
     def __init__(self):
         self.chain = []
-        self.curent_trx = []
+        self.current_trx = []
 
     def new_block(self):
         ''' create a new block '''
         pass
 
-    def new_trx(self):
+    def new_trx(self, sender, recipient, amount):
         ''' add a new trx to the mempool '''
-        pass
+        self.current_trx.append({'sender' : sender,
+                                 'recipient' : recipient,
+                                 'amount' : amount })
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
