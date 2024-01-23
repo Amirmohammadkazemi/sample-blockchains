c178026af0f67b33410f3da445bb3f47c1c84f0a
Amirmohammad Kazemi <amirmohammadkazemi2002@gmail.com>
2024-01-22 21:21:30 -0500

    start new block method

--- python-blockchain/py-blockchain.py
+++ python-blockchain/py-blockchain.py
@@ -1,15 +1,28 @@
+from time import time
+
 class blockchain():
     def __init__(self):
         self.chain = []
-        self.current_trx = []
+        self.current_trxs = []
+        self.new_block(previous_hash=1, proof=100)
 
     def new_block(self):
         ''' create a new block '''
-        pass
+        block = {
+            'index' : len(self.chain) + 1,
+            'timeStamp' : time(),
+            'trxs' : self.current_trxs,
+            'proof' : proof,
+            'previous_hash' : self.hash(self.chain[-1]),
+                }
+        self.current_trxs = []
+        self.chain.append(block)
+
+        return block
 
     def new_trx(self, sender, recipient, amount):
         ''' add a new trx to the mempool '''
-        self.current_trx.append({'sender' : sender,
+        self.current_trxs.append({'sender' : sender,
                                  'recipient' : recipient,
                                  'amount' : amount })
         return self.last_block['index'] + 1
